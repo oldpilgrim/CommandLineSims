8a9513f47b3571fe97185c0a8788e89533f24df0
Fred Chen <fredchen1994@outlook.com>
2020-11-08 16:45:21 +0000

    Add interrupt playback in SoundManager.cs

--- CommandLineSims/Program.cs
+++ CommandLineSims/Program.cs
@@ -3,11 +3,18 @@ using System.Media;
 
 namespace CommandLineSims
 {
-    class Program
+    public class Program
     {
+        public const bool IsDebug = true;
+        
         static void Main(string[] args)
         {
             new Game().Run();
+        }
+
+        public static void DebugLog(string message)
+        {
+            if (IsDebug) Console.WriteLine($"[DEBUG] {message}");
         }
     }
 }
--- CommandLineSims/SoundManager.cs
+++ CommandLineSims/SoundManager.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Media;
+using System.Threading;
 using System.Threading.Tasks;
+using static CommandLineSims.Program;
 
 namespace CommandLineSims
 {
@@ -10,7 +12,6 @@ namespace CommandLineSims
         // singleton. Field initialised
         // This would normally not be possible as game objects are instantiated by the game engine 
         private static readonly SoundManager Instance = new SoundManager();
-        private static readonly Random Random = new Random();
         private const string Path = "../../../audio/";
 
         private static readonly IReadOnlyList<string> Latin = new[]
@@ -22,28 +23,69 @@ namespace CommandLineSims
             "latin7.wav",
         };
         
+        private SoundManager() {}
+
         #region API
         
-        public static async void PlayNeighbourhood()
+        public static void PlayNeighbourhood()
         {
-            // https://stackoverflow.com/questions/37367104/check-when-sound-is-finished-c-sharp
-            // TODO interrupt
+            Instance._cts?.Cancel();
+            Instance._PlayNeighbourhood();
+        }
 
-            await Task.Run(() =>
-            {
-                // get random track
-                string location = Path + Latin[Random.Next(0, Latin.Count)];
-                Instance._musicPlayer.SoundLocation = location;
-                Instance._musicPlayer.PlaySync();
-            });
-            
-            // play on repeat
-            PlayNeighbourhood();
+        public static void PlayBuild()
+        {
+            Instance._cts?.Cancel();
+            Instance._PlayBuild();
         }
         
         #endregion
 
+        // fields should be non static where possible so can be automatically disposed
         private readonly SoundPlayer _musicPlayer = new SoundPlayer();
         private readonly SoundPlayer _sfxPlayer = new SoundPlayer();
+        private readonly Random _random = new Random();
+        
+        private CancellationTokenSource _cts;
+        
+        private async void _PlayNeighbourhood()
+        {
+            // https://stackoverflow.com/questions/37367104/check-when-sound-is-finished-c-sharp
+            // uniquely identifies the task to cancel. See https://docs.microsoft.com/en-us/dotnet/standard/parallel-programming/task-cancellation
+            _cts = new CancellationTokenSource();
+
+            Task playTask = Task.Run(() =>
+            {
+                _PlayRandomFromList(Latin);
+            }, _cts.Token);
+            
+            try
+            {
+                await playTask;
+            }
+            catch (TaskCanceledException e)
+            {
+                DebugLog("Play task cancelled");
+                return;
+            }
+
+            // play on repeat
+            _PlayNeighbourhood();
+        }
+
+        private async void _PlayBuild()
+        {
+            // TODO
+        }
+        
+        private void _PlayRandomFromList(IReadOnlyList<string> playlist)
+        {
+            int trackNum = _random.Next(0, playlist.Count);
+            string location = Path + playlist[trackNum];
+            DebugLog($"Playing track {trackNum} from playlist {nameof(playlist)}");
+            Instance._musicPlayer.SoundLocation = location;
+            Instance._musicPlayer.PlaySync();
+            DebugLog("Finished playing track");
+        }
     }
 }
